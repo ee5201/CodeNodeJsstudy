baaaa46c9a797266674d4a4ffe6fd352ae138e41
kyungmin kim <ee5201@naver.com>
2023-04-12 23:30:49 +0900

    updatestudy

---
+++ class/09-03-puppeteer-crawling-iframe/index.js
@@ -0,0 +1,28 @@
+import puppeteer from "puppeteer"
+
+async function startCrawling() {
+    const browser = await puppeteer.launch({ headless: false})
+    const page = await browser.newPage()
+    await page.setViewport({width:1280,height:720})
+    await page.goto("https://finance.naver.com/item/sise.naver?code=005930")
+    await page.waitForTimeout(1000)
+    const framePage = await page.frames().find((el)=>(el.url().includes("/item/sise_time.naver?code=005930")))
+
+  for(let i=3; i<=7; i++){
+    const data = await framePage.$eval(`body > table.type2 > tbody > tr:nth-child(${i}) > td:nth-child(1)`,(el)=>
+      el.textContent
+    )
+    const price = await framePage.$eval(`body > table.type2 > tbody > tr:nth-child(${i}) > td:nth-child(2) > span`,(el)=>
+      el.textContent
+    )
+    console.log(`날짜: ${data},가격:${price}`)
+    
+
+  }
+
+
+
+await browser.close()
+}
+
+startCrawling()
